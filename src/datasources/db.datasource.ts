2f1a9d6c10a8e44621323403499b02755fc798de
eelok <maria@resourceful-humans.de>
2023-04-12 21:38:39 +0200

    change port

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -6,7 +6,7 @@ const config = {
   connector: 'postgresql',
   url: '',
   host: 'localhost',
-  port: 5432,
+  port: 5436,
   user: 'postgres',
   password: 'postgres',
   database: 'phone-factory-lb4'
